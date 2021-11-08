4f4af7fe9ba32c93a9cea9c27a05c820b8cc0275
ErickYT20 <88303753+ErickYT20@users.noreply.github.com>
2021-11-08 16:33:10 +0530

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,9 @@
+module.exports = (client) => {
+    console.log(`┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓`)
+console.log(`┃Logging into ${client.user.tag}┃`)
+console.log(`┃Coded by Miku                  ┃`)
+console.log(`┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓`)
+console.log(`┃Logged into ${client.user.tag} ┃`)
+console.log(`┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛`);
+    
+};
